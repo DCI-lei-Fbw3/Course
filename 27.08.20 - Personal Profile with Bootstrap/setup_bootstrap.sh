53ca164116ba3c4aa3c30eac2a6973bf5c5d1ed8
Thomas Hofmann <thomas@savoy-truffle.de>
2020-08-27 10:01:50 +0200

    more verbose output in setup-script

--- 27.08.20 - Personal Profile with Bootstrap/setup_bootstrap.sh
+++ 27.08.20 - Personal Profile with Bootstrap/setup_bootstrap.sh
@@ -2,10 +2,15 @@
 
 set -e
 
+echo "retrieving bootstrap"
 wget https://github.com/twbs/bootstrap/releases/download/v4.5.2/bootstrap-4.5.2-dist.zip -O bootstrap.zip
 
+echo "unzipping the downloaded file"
 unzip bootstrap.zip
 
+echo "create a symlink to reference from html-pages"
+echo " - so that on an update the html-pages do not need to be changed"
 ln -s bootstrap-4.5.2-dist bootstrap
 
+echo "cleanup: removing the downloaded zip-file"
 rm bootstrap.zip
