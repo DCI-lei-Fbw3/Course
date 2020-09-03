ab872f149aa0fcd8edde88f1eef8b2baace52800
Thomas Hofmann <thomas@savoy-truffle.de>
2020-09-03 16:47:44 +0200

    restructuring of repository

---
+++ 02-Programming Basics/2020-09-03/exercises/exercises.js
@@ -0,0 +1,11 @@
+text = "understanding";
+console.log(text);
+
+conmouseleave.log(text[3], text [5]);
+console.log(text.charAt(1));
+
+name = "will";
+age = 52;
+year = 1995;
+console.log(name * " is " + age + "in " + year);
+
