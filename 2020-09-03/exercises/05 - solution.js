b2e50adc0f969e03e53f2b164550570b3828e7f8
Thomas Hofmann <thomas@savoy-truffle.de>
2020-09-03 11:26:55 +0200

    created solution 05

---
+++ 2020-09-03/exercises/05 - solution.js
@@ -0,0 +1,38 @@
+
+//1 
+a = "hi there"
+
+console.log(a)
+
+//2 
+console.log(a[1])
+console.log(a[5])
+
+// 3
+name = "Thomas"
+//4 
+age = 45
+//5
+year = 2020
+//6
+console.log(name + " is " + age + " in " + year)
+console.log(`${name} is ${age} in ${year}`)
+
+//7
+isMarried = false
+//8
+console.log(`${name} is married: ${isMarried}`)
+
+//9
+programmingLanguage = "JavaScript"
+isFun = true
+console.log(`${programmingLanguage} is fun: ${isFun}`)
+
+//10
+console.log(programmingLanguage[0] + programmingLanguage[4])
+
+//11
+console.log("the variable programmingLanguage holds a string with length: " + programmingLanguage.length)
+console.log(`the variable programmingLanguage holds a string with length: ${programmingLanguage.length}`)
+
+console.log("The string \"test\" has the length: " + "test".length)
