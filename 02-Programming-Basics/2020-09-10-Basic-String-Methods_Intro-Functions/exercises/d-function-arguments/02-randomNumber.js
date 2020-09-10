62001b9b37f373e4275cd04997975686438da02d
Thomas Hofmann <thomas@savoy-truffle.de>
2020-09-10 15:18:24 +0200

    custumized exercise

--- 02-Programming-Basics/2020-09-10-Basic-String-Methods_Intro-Functions/exercises/d-function-arguments/02-randomNumber.js
+++ 02-Programming-Basics/2020-09-10-Basic-String-Methods_Intro-Functions/exercises/d-function-arguments/02-randomNumber.js
@@ -1,14 +1,15 @@
 /*
 
-    - create a two functions "randomFloat" and "randomInteger"
-    - they take two arguments (min, max)
+    - create a function "randomInteger"
+    - that function takes two arguments (min, max)
       only positive integers allowed, but no test is required, 
       assume "min" and "max" are always positive integers including 0
-      also assume that "max" is bigger than "min"
+      also assume that "max" is bigger than "min", - also not test for this
     - it should return an integer between and including "min" and "max"
 
-    - output some random numbers with min=0 and max=1
-    - output some random numbers with min=0 and max=100
-    - output some random numbers with min=1 and max=6
+    - use the function "randomInteger" to: 
+      - output some random numbers with min=0 and max=1
+      - output some random numbers with min=0 and max=100
+      - output some random numbers with min=1 and max=6
 
 */
