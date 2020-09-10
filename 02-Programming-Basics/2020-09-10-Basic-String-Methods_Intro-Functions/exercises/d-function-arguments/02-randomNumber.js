8ea3b6c2008acec7f3a7845cdcc8e4774bdc272a
Thomas Hofmann <thomas@savoy-truffle.de>
2020-09-10 16:23:02 +0200

    solution for 02

--- 02-Programming-Basics/2020-09-10-Basic-String-Methods_Intro-Functions/exercises/d-function-arguments/02-randomNumber.js
+++ 02-Programming-Basics/2020-09-10-Basic-String-Methods_Intro-Functions/exercises/d-function-arguments/02-randomNumber.js
@@ -11,4 +11,34 @@
       - output some random numbers with min=0 and max=1    => 1, 0, 0, 1
       - output some random numbers with min=0 and max=100
       - output some random numbers with min=1 and max=6    => like a dice
+
+    - Try to call "randomInteger" the following ways, see what is output:
+      - randomInteger(-1, 0)
+      - randomInteger(-10, 10)
 */
+
+console.log("[0, 1]:\t\t",   randomInteger(0, 1));
+console.log("[0, 100]:\t", randomInteger(0, 100));
+console.log("[1, 6]:\t\t",   randomInteger(1, 6));
+
+console.log("The math also works for negative numbers :-)");
+console.log("[-1, 0]:\t",   randomInteger(-1, 0));
+console.log("[-10, -8]:\t",   randomInteger(-10, -8));
+console.log("[-10,-10]:\t",   randomInteger(-10, 10));
+
+function randomInteger(min, max) {
+  let rand = Math.random();
+
+  let scalingFactor = max + 1 - min;
+
+  // *** stepwise ***
+  // let scaledResult = scalingFactor * rand;
+  // let translatedResult = scaledResult + min;
+  // let result = Math.floor(translatedResult);
+  // return result;
+
+  // *** the math in one step ***
+  //                         scaling the random number  and moving it by "min"
+  let result2 = Math.floor(scalingFactor * Math.random() + min)
+  return result2;
+}
