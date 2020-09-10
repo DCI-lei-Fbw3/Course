c5607c09b9365ee532087fdb7c95e3b6c479e8ff
Thomas Hofmann <thomas@savoy-truffle.de>
2020-09-10 12:53:03 +0200

    exercises aroung arguments/parameters, one of them more involved into random numbers

---
+++ 02-Programming-Basics/2020-09-10-Basic-String-Methods_Intro-Functions/exercises/d-function-arguments/01-showGreeting.js
@@ -0,0 +1,8 @@
+/* 
+    create a function named showGreeting
+    show greeting has one optional argument "name"
+    it should log "Hello " + name to the console if a name is given, 
+    else "Hello World".
+
+    present the functionality of that function, with and without an argument
+*/ 
---
+++ 02-Programming-Basics/2020-09-10-Basic-String-Methods_Intro-Functions/exercises/d-function-arguments/02-randomNumber.js
@@ -0,0 +1,14 @@
+/*
+
+    - create a two functions "randomFloat" and "randomInteger"
+    - they take two arguments (min, max)
+      only positive integers allowed, but no test is required, 
+      assume "min" and "max" are always positive integers including 0
+      also assume that "max" is bigger than "min"
+    - it should return an integer between and including "min" and "max"
+
+    - output some random numbers with min=0 and max=1
+    - output some random numbers with min=0 and max=100
+    - output some random numbers with min=1 and max=6
+
+*/
