7d708aaef3c3c23865d2e7989d9fb03e739ef42e
Thomas Hofmann <thomas@savoy-truffle.de>
2020-09-10 15:30:18 +0200

    correct wording

--- 02-Programming-Basics/2020-09-10-Basic-String-Methods_Intro-Functions/exercises/d-function-arguments/02-randomNumber.js
+++ 02-Programming-Basics/2020-09-10-Basic-String-Methods_Intro-Functions/exercises/d-function-arguments/02-randomNumber.js
@@ -1,15 +1,14 @@
 /*
 
     - create a function "randomInteger"
-    - that function takes two arguments (min, max)
+    - that function has two parameter (min, max)
       only positive integers allowed, but no test is required, 
       assume "min" and "max" are always positive integers including 0
       also assume that "max" is bigger than "min", - also not test for this
     - it should return an integer between and including "min" and "max"
 
     - use the function "randomInteger" to: 
-      - output some random numbers with min=0 and max=1
+      - output some random numbers with min=0 and max=1    => 1, 0, 0, 1
       - output some random numbers with min=0 and max=100
-      - output some random numbers with min=1 and max=6
-
+      - output some random numbers with min=1 and max=6    => like a dice
 */
