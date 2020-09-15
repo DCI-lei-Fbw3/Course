52ad517a09c3f9fcbb2d2131dadff761d3049e17
Thomas Hofmann <thomas@savoy-truffle.de>
2020-09-15 19:51:49 +0200

    exercises for 2020-09-16

---
+++ 02-Programming-Basics/2020-09-16-conditionals_and_repeating/exercises/06-while-break.js
@@ -0,0 +1,5 @@
+console.log("loop over i as long as it is less than 100.")
+console.log("in the loop:");
+console.log("- leave the loop if 'i' is divisible by 42.");
+console.log("- log i to the console");
+console.log("- increment i by 1");
