adc3844275767246e3e62c8007edc31e18e10f85
Thomas Hofmann <thomas@savoy-truffle.de>
2020-09-16 12:19:00 +0200

    more exercises for for-loops

---
+++ 02-Programming-Basics/2020-09-16-conditionals_and_repeating/exercises/09-print_three_random_numbers.js
@@ -0,0 +1,12 @@
+// you could directly solve the last exercise, 
+// but the stepwise approach shows how the problem can be approached.
+
+console.log("print a random number");
+
+console.log("print 3 random numbers");
+
+console.log("print 3 random numbers between 0 and 5")
+
+console.log("print 3 random numbers between 1 and 6");
+
+
