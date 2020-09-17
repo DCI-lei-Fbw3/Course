c7ffb4c085c6c78a499e77d2ec26259d49457812
Thomas Hofmann <thomas@savoy-truffle.de>
2020-09-17 12:00:08 +0200

    updated content of 03-array-write.js

--- 02-Programming-Basics/2020-09-17/demo/03-array-write.js
+++ 02-Programming-Basics/2020-09-17/demo/03-array-write.js
@@ -1,1 +1,18 @@
 // 5 random numbers
+
+let numbers = [];
+
+for (let i = 0; i < 5; i++) {
+  //numbers[i] = Math.random();
+  //numbers.push(Math.random());
+  numbers.push(i);
+}
+
+// for (let i = 0; i < 5; i++) {
+//   console.log(numbers[i]);
+// }
+
+while (numbers.length > 0) {
+  // console.log(numbers.pop());
+  console.log(numbers.shift());
+}
