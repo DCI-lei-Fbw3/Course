ef2e0e792679ffad484335eb8d2b8d7a791a2eaf
Thomas Hofmann <thomas@savoy-truffle.de>
2020-09-17 11:00:38 +0200

    updated wording

---
+++ 02-Programming-Basics/2020-09-17/exercises/03-create-pairs.js
@@ -0,0 +1,21 @@
+/*
+
+Exercise: "Setup Pairs for Pair Programming"
+
+Imagine a class of students.
+In order to setup pairs of students for pair programming
+an overview of possible pairs should be provided.
+
+Store the pairs in a variable.
+Finally output those pairs.
+
+What variables are needed?
+What are the data structures of those variables?
+
+Bonus:
+In some circumstances it might be necessary, to prohibit certain pairs, 
+let's assume, no one of the first three students should ever be in the same pair.
+
+How could this be solved?
+
+*/
