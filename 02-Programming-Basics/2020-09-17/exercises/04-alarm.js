59b24bd41fb5384153e2e06ea38b46a8f200514b
Thomas Hofmann <thomas@savoy-truffle.de>
2020-09-17 10:37:35 +0200

    added exercise alarm

---
+++ 02-Programming-Basics/2020-09-17/exercises/04-alarm.js
@@ -0,0 +1,16 @@
+/*
+
+Generate 10 random Numbers between [0, 1[  - that is 0 <= NUMBER < 1
+
+define a const THRESHOLD and assign a value between the former interval
+
+Loop over the generated numbers and log an alarm with the value and THRESHOLD, like:
+"Attention: current value (VALUE) exceeds the threshold (THRESHOLD) by DIFFERENCE."
+
+Bonus:
+For each 0.1 of difference add an "*" at the start of the logged line, e. g. 
+"*** Attention: current value (0.67859) exceeds the threshold (0.3) by 0.37859."
+
+To determine the number of "*", the difference may be rounded down.
+
+*/
