75534366324577bcf9292379f5285105c5b0d726
Shih-chen Chao <cybersagittarius@gmail.com>
2020-09-17 10:11:42 +0200

    first committing to chessboard

--- 02-Programming-Basics/2020-09-17/exercises/02-chessboard.js
+++ 02-Programming-Basics/2020-09-17/exercises/02-chessboard.js
@@ -11,7 +11,20 @@
 " # # # #"
 
 */
+for (let i=0; i<=7; i++)
+{
+    for (let j=0; j<=7; j++)
+    {
+       if (i==0 && j%2!=0){
+        console.log("!!!");
+    }
+       if (i%2==0 && j==7){
+        console.log("???");
+       }
 
+        console.log(i*j); 
+    }
+}
 /*
 
 2) make the height and width configurable
