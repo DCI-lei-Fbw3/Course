119c390fa181d3f867f9b2dbb5ecc214260a2a7f
Thomas Hofmann <thomas@savoy-truffle.de>
2020-09-23 09:14:12 +0200

    fixed typo

--- 02-Programming-Basics/2020-09-22-funtions-intro/intro-functions.js
+++ 02-Programming-Basics/2020-09-22-funtions-intro/intro-functions.js
@@ -11,13 +11,13 @@ Some initial thoughts on Functions:
 d
 */
 
-        function makeNoise () {
-        console.log("Pling!")
-        };
+function makeNoise() {
+  console.log("Pling!");
+}
 
-        makeNoise(); // --> call statement
+makeNoise(); // --> call statement
 
-        /*
+/*
         Things we have done so far:
 
         you start a function by using keywords "function"
@@ -29,7 +29,7 @@ d
 
         callFunctionLikeThis();
 
-        //Another way of wrting functions, is by storing them ins constants
+        //Another way of wrting functions, is by storing them in constants
         //However, it does the same in the first method as well.
 
         const makeNoise2 = function() {
@@ -62,111 +62,107 @@ when a function does not produce a value, but an action (such as the console.log
 in the  makeNoise function) then this action is called a "side effect"
 */
 
-// Scope of functions 
+// Scope of functions
 
-        const half = function (n) { // this is the local n
-            return n/2;
-        };
-        let n = 10; // this is the global n
+const half = function (n) {
+  // this is the local n
+  return n / 2;
+};
+let n = 10; // this is the global n
 
-        console.log(half(100)); // value returned = 50
+console.log(half(100)); // value returned = 50
 
-        console.log(n); // value 10
+console.log(n); // value 10
 
+//Function declarations
 
- //Function declarations
+// 1. Declaration Notation
 
-        // 1. Declaration Notation
-    
-    function functionName (parameters){
-            //the function body, which is whithin the function scope
+function functionName(parameters) {
+  //the function body, which is whithin the function scope
+}
 
-    }
+// 2. Functions as Values
 
-        // 2. Functions as Values
+// You can pass funcitons as values to variables, as well as other functions
+const someConstant = function (parameters) {
+  //the function body, which is whithin the function scope
+};
 
-    // You can pass funcitons as values to variables, as well as other functions
-    const someConstant = function(parameters) {
-            //the function body, which is whithin the function scope
+// 3. Arrow Functions
 
-    }
+// the arrow sign is denoted by =>
+// DO NOT confuse with "greater than or equal to" operator (>=)
+// "this input (the parameters) produces this result (the body)"
 
-        // 3. Arrow Functions
+const power = (base, exponent) => {
+  let result = 1;
+  for (let i = 0; i < exponent; i++) {
+    result = result * base;
+  }
+  return result;
+};
 
-        // the arrow sign is denoted by =>
-        // DO NOT confuse with "greater than or equal to" operator (>=)
-        // "this input (the parameters) produces this result (the body)"
-
-        const power = (base, exponent) => {
-            let result = 1;
-            for (let i = 0; i < exponent ; i++) {
-                result = result * base;
-                }
-            return result;
-        }
-
-        /* 
+/* 
         You can omit the parenthesis around the parameter, 
         only (and only if) you have a single parameter
         */
-        
-        const square1 = (x) => { return x*x};
-        const square2 = x =>  x*x;
+
+const square1 = (x) => {
+  return x * x;
+};
+const square2 = (x) => x * x;
 
 // The Callstack
 
-        // the Callstack is the way control flows through functions.
+// the Callstack is the way control flows through functions.
 
-    /*
+/*
     You must have heard about the very common thought experiment,
     asking whether the chicken or the egg came first 
      */
 
-     function chicken() {
-         egg();
-     }
-     function egg() {
-         chicken();
-     }
+function chicken() {
+  egg();
+}
+function egg() {
+  chicken();
+}
 
-     egg();
+egg();
 
-     // this here is what's called "Circular Dependencies"
+// this here is what's called "Circular Dependencies"
 
-     /* 
+/* 
      Once you return a function, the execution of your script, goes back
      to where the function is being called
      
      */
 
-    console.log(power3(2,4));
-    console.log(power1(3,5)); 
-    console.log(power2(5,7));
+console.log(power3(2, 4));
+console.log(power1(3, 5));
+console.log(power2(5, 7));
 
+function power1(base, exponent) {
+  let result = 1;
+  for (let i = 0; i < exponent; i++) {
+    result = result * base;
+  }
+  return result;
+}
 
-        function power1 (base, exponent) {
-        let  result = 1;
-        for (let i = 0; i < exponent ; i++) {
-            result = result * base;
-            }
-        return result;
-        }                      
-  
+function power2(base, exponent) {
+  let result = 1;
+  for (let i = 0; i < exponent; i++) {
+    result = result * base;
+  }
+  return result;
+}
 
-
-        function power2 (base, exponent) {
-            let  result = 1;
-            for (let i = 0; i < exponent ; i++) {
-                result = result * base;
-                }
-            return result;
-            }     
-
-            
-         function power3 (base, exponent) {
-            let  result = 1;
-            for (let i = 0; i < exponent ; i++) {
-                result = result * base;
-                    }
-                return result;
-                }     
+function power3(base, exponent) {
+  let result = 1;
+  for (let i = 0; i < exponent; i++) {
+    result = result * base;
+  }
+  return result;
+}
