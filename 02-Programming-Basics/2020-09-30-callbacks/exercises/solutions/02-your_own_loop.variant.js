5e82f30f39f5b078aa87ba875575dba20174f433
Thomas Hofmann <thomas@savoy-truffle.de>
2020-10-01 14:06:55 +0200

    more variants on the 'your own loop'-exercise

--- 02-Programming-Basics/2020-09-30-callbacks/exercises/solutions/02-your_own_loop.variant.js
+++ 02-Programming-Basics/2020-09-30-callbacks/exercises/solutions/02-your_own_loop.variant.js
@@ -1,5 +1,5 @@
 function loop(value, testFunction, updateFunction, bodyFunction) {
-	while (testFunction(test)) {
+	while (testFunction(value)) {
 		//  ! false
 		//if (!testFunction(value)) break;
 
@@ -9,12 +9,44 @@ function loop(value, testFunction, updateFunction, bodyFunction) {
 	}
 }
 
+function loopWithFor(value, testFunction, updateFunction, bodyFunction) {
+	for (value; testFunction(value); value = updateFunction(value)) {
+		bodyFunction(value);
+	}
+}
+
+// const testFn = (n) => n > 0;
+// const testFn = (n) => {
+// 	return n > 0;
+// };
+
+// const testFn = function (n) {
+// 	return n > 0;
+// };
+
+function testFn(n) {
+	return n > 0;
+}
+
+function updateFn(n) {
+	return n - 1;
+}
+
+console.log("loop(inplace-definitions):");
 loop(
 	3,
 	(n) => n > 0,
 	(n) => n - 1,
 	console.log
 );
+
+console.log("loop(3, testFn, updateFn, console.log):");
+loop(3, testFn, updateFn, console.log);
+
+console.log("loopWithFor(...):");
+loopWithFor(3, testFn, updateFn, console.log);
+
+//const updateFn
 
 // 3
 // 2
