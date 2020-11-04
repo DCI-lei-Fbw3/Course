1c74ac30bba3bff3193a9b93d669353fb7aceb79
Thomas Hofmann <thomas@savoy-truffle.de>
2020-11-04 11:45:47 +0100

    hints on debugging (print-debugging/debugger)

--- 02-Programming-Basics/2020-11-04 - Errors/demo/30-logic.js
+++ 02-Programming-Basics/2020-11-04 - Errors/demo/30-logic.js
@@ -1,6 +1,7 @@
 // source:
 // https://eloquentjavascript.net/08_error.html
 
+// return the given "n" transformed from being a decimal number to a number of the given "base"
 function numberToString(n, base = 10) {
     let result = "", sign = "";
     if (n < 0) {
@@ -9,14 +10,23 @@ function numberToString(n, base = 10) {
     }
     do {
         result = String(n % base) + result;
-        n /= base;
-        //n = Math.floor(n / base);
+        //console.log(n, ":", n % base); // console.log/print debugging
+        // try instead of print-debugging the debugger with a breakpoint
+        n = n / base;
+        //n = Math.floor(n / base); // fix
     } while (n > 0);
 
     return sign + result;
 }
 
-console.log(numberToString(13, 10));
+/*
+8421
+0001
+     8421
+6 => 0110
+*/
+
+console.log(numberToString(6, 2));
 // → 1.5e-3231.3e-3221.3e-3211.3e-3201.3e-3191.3e-3181.3…
 
 /*
