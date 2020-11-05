93418493b0e03e3c89c100e120199c222c928572
Thomas Hofmann <thomas@savoy-truffle.de>
2020-11-05 10:22:58 +0100

    work on tests

--- 02-Programming-Basics/2020-11-05-Exceptions-and-Tests/demo/10-tests/10-tests.js
+++ 02-Programming-Basics/2020-11-05-Exceptions-and-Tests/demo/10-tests/10-tests.js
@@ -3,14 +3,14 @@
 
 function test(label, body) {
     if (!body()) console.log(`Failed: ${label}`);
-  }
+}
 
-  test("convert Latin text to uppercase", () => {
-    return "hello".toUpperCase() == "HELLO";
-  });
-  test("convert Greek text to uppercase", () => {
-    return "Χαίρετε".toUpperCase() == "ΧΑΊΡΕΤΕ";
-  });
-  test("don't convert case-less characters", () => {
-    return "مرحبا".toUpperCase() == "مرحبا";
-  });
+test("convert Latin text to uppercase", () => {
+  return "hello".toUpperCase() == "HELLO";
+});
+test("convert Greek text to uppercase", () => {
+  return "Χαίρετε".toUpperCase() == "ΧΑΊΡΕΤΕ";
+});
+test("don't convert case-less characters", () => {
+  return "مرحبا".toUpperCase() == "مرحبا";
+});
