1528c77d24ad029185e97b43e1251c6bb2b4c8f1
Thomas Hofmann <thomas@savoy-truffle.de>
2020-11-05 13:40:12 +0100

    added export/import of multiple functions

--- 02-Programming-Basics/2020-11-05-Exceptions-and-Tests/demo/10-tests/20-unit-tests/sum.test.js
+++ 02-Programming-Basics/2020-11-05-Exceptions-and-Tests/demo/10-tests/20-unit-tests/sum.test.js
@@ -1,11 +1,11 @@
-const sum = require('./sum');
+const {sum, sumAlt} = require('./sum');
 
-test('adds 1 + 2 to equal 3', () => {
+test('sum: 1 + 2 to equal 3', () => {
 expect(sum(1, 2)).toBe(3);
 });
 
-test('adds -1 + 2 to equal 1', () => {
-	expect(sum(-1, 2)).toBe(1);
+test('sumAlt: 1 + 2 to equal 3', () => {
+	expect(sumAlt(1, 2)).toBe(3);
 });
 
 //test('adds 1 + 2 to equal 3', () => {
