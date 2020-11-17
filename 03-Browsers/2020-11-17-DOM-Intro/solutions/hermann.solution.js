cb47e79ca198f38ced7e852eac17fe9e7692d992
Hermann-237 <61315223+Hermann-237@users.noreply.github.com>
2020-11-17 16:14:54 +0100

    Create hermann.solution.js

---
+++ 03-Browsers/2020-11-17-DOM-Intro/solutions/hermann.solution.js
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>01 Window Object</title>
+</head>
+<body>
+    <h1 style="text-align: center; color: red;padding-top: 200px;">Start plying the Game!!!!!!</h1>
+
+    <script >
+
+        /* SOLUTION USING FOR LOOP*/
+
+const game = ()=>{
+    let ramdomNumber = Math.round(Math.random()*10);
+let enterNumber = window.prompt('Guess a number between 1 - 10');
+
+for(let i = 0; i < 3; i++){
+    if(enterNumber != ramdomNumber){
+        enterNumber =  window.prompt('Wrong!!! Try Again');
+    }
+}
+
+if(enterNumber == ramdomNumber){
+    window.alert(`Success, the number was ${enterNumber}! Attempts : ${ramdomNumber}` );
+}else{
+    window.alert(`Sorry,you faliled to guess the number in  three attempts. The number was ${ramdomNumber}` );
+}
+}
+
+game();
+
+
+
+
+/* SOLUTION USING WHILE LOOP*/
+
+/* 
+const game = ()=>{
+    let ramdomNumber = Math.round(Math.random()*10);
+let essais = 3;
+let enterNumber = window.prompt('Guess a number between 1 - 10');
+while(essais > 0){
+    essais--
+    
+    if(enterNumber != ramdomNumber){
+        enterNumber =  window.prompt('Wrong!!! Try Again');
+    }
+}
+if(enterNumber == ramdomNumber){
+    window.alert(`Success, the number was ${enterNumber}! Attempts : ${ramdomNumber}` );
+}else{
+    window.alert(`Sorry,you faliled to guess the number in  three attempts. The number was ${ramdomNumber}` );
+}
+}
+
+game();  */
+
+
+
+    </script>
+
+</body>
+</html>
