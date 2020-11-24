754efb1fa77a0e13328076e4c864906c49058835
NewUsername1234 <ilja.romanov@yahoo.com>
2020-11-24 16:25:17 +0100

    update solution 1

--- 03-Browsers/2020-11-24-DOM-Traversing/solutions/ilya/01-DOM-traversing-master/assets/js/index.js
+++ 03-Browsers/2020-11-24-DOM-Traversing/solutions/ilya/01-DOM-traversing-master/assets/js/index.js
@@ -1,13 +1,13 @@
 document.getElementsByTagName('h1')[0].closest('header').style.border = '5px solid grey';
 
-if (document.querySelector('section.info .info-package') !== null) {
+if (document.querySelector('section.info .info-package')) {
     [...document.getElementsByClassName('package-title')].forEach(title =>
         title.previousElementSibling.style.border = '2px solid midnightblue')
 }
 
 [...document.getElementsByTagName('label')].forEach(label => {
 
-    switch (label.textContent.toLowerCase()) {
+    switch (label.className) {
         case 'mild':
             label.style.borderBottom = '2px solid yellow';
             break;
@@ -23,3 +23,11 @@ if (document.querySelector('section.info .info-package') !== null) {
 
 [...document.getElementsByClassName('nav-list')[0].children].forEach(li => 
     document.getElementsByClassName('site-map')[0].appendChild(li.cloneNode(true)));
+
+// alternative:
+/* 
+let navList = document.querySelector(".nav-list");
+let footerList = document.querySelector(".site-map");
+
+footerList.innerHTML = navList.innerHTML; 
+*/
