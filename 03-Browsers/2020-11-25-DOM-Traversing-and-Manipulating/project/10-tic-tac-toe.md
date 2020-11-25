56c06674b8b1f8bdeb5c4d1290981ae22f7c4cc3
Thomas Hofmann <thomas@savoy-truffle.de>
2020-11-25 12:23:15 +0100

    initial commit on the tic-tac-toe project

---
+++ 03-Browsers/2020-11-25-DOM-Traversing-and-Manipulating/project/10-tic-tac-toe.md
@@ -0,0 +1,6 @@
+# Tic Tac Toe
+
+Start creating a tic, tac, toe game in the browser.
+
+- some visualization, - start with a simple one
+- we need functions, that move around
---
+++ 03-Browsers/2020-11-25-DOM-Traversing-and-Manipulating/project/tic-tac-toe/tic-tac-toe.js
@@ -0,0 +1,20 @@
+let board = document.getElementById("board");
+
+board.innerHTML = '<table>'
++ '<tr>'
++ '	<td>11</td>'
++ '	<td>12</td>'
++ '	<td>13</td>'
++ '</tr>'
++ '<tr>'
++ '	<td>21</td>'
++ '	<td>22</td>'
++ '	<td>23</td>'
++ '</tr>'
++ '<tr>'
++ '	<td>31</td>'
++ '	<td>32</td>'
++ '	<td>33</td>'
++ '</tr>'
++ '</table>';
+
