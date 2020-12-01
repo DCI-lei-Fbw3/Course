329d06edf3259347a16e6c35120adc9dec00e4ae
Shih-chen Chao <cybersagittarius@gmail.com>
2020-12-01 13:13:21 +0100

    added solution to the exercise 01_12_2020 locally

--- 03-Browsers/2020-11-26-DOM-and-Events-Intro/project/myTicTacToe.js
+++ 03-Browsers/2020-11-26-DOM-and-Events-Intro/project/myTicTacToe.js
@@ -51,11 +51,20 @@
 //3. add a div container in html with an id="board"
 //4. get the container in js using getElement by ID, and add the table to it using append()? 
 
+// steps: 
+
+//1. Create a 3x3 table in html 
+//2. style it with borders and padding in css 
+//3. add a div container in html with an id="board"
+//4. get the container in js using getElement by ID, and add the table to it using append()? 
+
 class Game {
     // we create a constructor class that takes two arguments: rows & columns, which will be used to determine the dimensions of the tables; 
     constructor(rows, columns) {
         this.rows = rows;
         this.columns = columns;
+        this.setOfPieces = ["X", "O"];
+        this.history = [];
     }
     // the board() method will create the tables: 
     board() {
@@ -76,34 +85,96 @@ class Game {
                 //we attach the TDs to the TR variable: 
                 tr.append(td);            
             }
-        } 
-        
+        }         
         return table
     }
 
-    generateIDName(rowNum, cellNum) {
-        // 112 => x = 11, y =  2
-        // or  => x =  1, y = 12
-        // 11_2 => x=11, y=2
+    generateIDName(rowNum, cellNum) {       
         return rowNum  + "_" + cellNum;
     }
 
-    mark(rowNum, cellNum, str) {
-        let id = this.generateIDName(rowNum,cellNum); // get the string being used as the ID.
-        let element = document.getElementById(id);
-        element.textContent = str;
+    // mark(rowNum, cellNum, str) {
+    //     let id = this.generateIDName(rowNum, cellNum); // get the string being used as the ID.
+    //     let element = document.getElementById(id);
+    //     element.textContent = str;
+    // }
+    
+    //create the second box which carries the source of x and o for copying and comparing
+    pieces(){
+        let table = document.createElement("table");
+
+        let row = document.createElement("tr");
+        table.append(row);
+
+       for (let i in this.setOfPieces) { 
+            let td = document.createElement("td");
+            let type = this.setOfPieces[i];
+		        td.id = type;
+			      td.className = "piece_" + type;
+			    row.append(td);
+		    }             
+        return table;
+    }    
+
+    //the copy method copies the css-class
+    copy(source, target) {
+        target.classList.value = " ";
+        source.classList.forEach((cssClass) => {
+        target.classList.add(cssClass);
+      });
     }
+       
+    //move source to target
+    move (source, target) {
+      target.classList.value = " ";
+		  source.classList.forEach((cssClass) => {
+			console.log(cssClass);
+			source.classList.remove(cssClass); 
+			target.classList.add(cssClass);
+		});
+	}
+    
+    //a new div container with the id of "game" was created in html. 
+    makeOnClickHandler() {
+        let gameBox = document.getElementById("game");
+        gameBox.addEventListener("click", (anEvent) =>
+          this.onClickHandler(anEvent)
+        );
+      }
+
+      onClickHandler(anEvent) {
+        this.history.push(anEvent.target); 
+        
+        if (this.history.length == 2) {
+            
+          let sourceElement = this.history[0];
+          let targetElement = this.history[1];
+          let sourceSection = sourceElement.closest("div").id; 
+    
+          if (sourceSection == "pieces") {
+            this.copy(sourceElement, targetElement);
+          } else if (sourceSection == "board") {
+            this.move(sourceElment, targetElment);
+          }    
+          this.history = [];
+        }
+      }
 } 
 
 let g = new Game(3, 3);
 
 let board = document.getElementById('board');
 board.textContent = ""; // to get rid of the text which is contained in the div-container, refer to the index.html
-let boardTable = g.board();
-board.append(boardTable);
+board.append(g.board());
+board.addEventListener("click", g.makeOnClickHandler());
 
-g.mark(1, 1, "x");
-g.mark(2, 2, "o");
+let pieces = document.getElementById('pieces');
+pieces.textContent = "";
+pieces.append(g.pieces());
+//pieces.addEventListener("click", g.makeOnClickHandler());
+
+
+
 
 
 
