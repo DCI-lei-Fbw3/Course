549a9d9fdf2f5d31001c827d44480822e6a02935
Raafat <slipknot9966@gmail.com>
2020-12-02 10:34:31 +0100

    adding mouseleave

--- 03-Browsers/2020-12-01-EventObject/solutions/raafat/toggleItems-exercise/app.js
+++ 03-Browsers/2020-12-01-EventObject/solutions/raafat/toggleItems-exercise/app.js
@@ -7,9 +7,10 @@ for (const item of items) {
 	item.addEventListener("click", (event) => {
 		let selectItem = event.target.innerHTML;
 		alert.classList.remove("d-none");
-		alert.innerHTML = `<p>you have selected ${selectItem}</p>`;
+		alert.innerHTML = `you have selected ${selectItem}`;
 	});
 }
 button.addEventListener("click", (event) =>
 	groupList.classList.toggle("d-none")
 );
+groupList.addEventListener("mouseleave", () => alert.classList.add("d-none"));
