5ab1586b6d6124a443686e0f8b8eb001348bc689
cybersagittarius <31215917+cybersagittarius@users.noreply.github.com>
2020-12-07 09:34:42 +0100

    Delete index.js

--- 03-Browsers/2020-11-25-DOM-Traversing-and-Manipulating/exercises/10-dom-traversing-parents/index.js
+++
@@ -1,9 +0,0 @@
-let mainArr = document.querySelectorAll("html, body, body *");   
-  
-  // convert to an array using Array.from() 
-let collectionArray = [];
-Array.from(mainArr).forEach(function(element) { 
-           collectionArray.push(element);
-     }); 
-   console.log(collectionArray);
-    
