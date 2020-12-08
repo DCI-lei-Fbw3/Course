42330f712a0027046a25d312842b533a4f5e7d59
Raafat <slipknot9966@gmail.com>
2020-12-08 09:48:20 +0100

    finishing modules

---
+++ 03-Browsers/2020-12-07-Modules/solutions/raafat/Browser-modules-basics/modules/click.js
@@ -0,0 +1,11 @@
+function clickImages() {
+    const sectionContent = document.getElementById("content");
+    sectionContent.addEventListener("click", e => {
+        switch (e.target.tagName) {
+            case "IMG":
+            alert("All of the yarn is available in our shop!")
+        }
+    })
+ }
+ 
+ export {clickImages};
