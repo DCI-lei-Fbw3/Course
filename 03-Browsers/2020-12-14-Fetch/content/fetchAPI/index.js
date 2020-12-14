2f83c7762a42c1f5407bdd26517c581471739bd8
thisistaimur <59065240+thisistaimur@users.noreply.github.com>
2020-12-14 10:44:01 +0100

    Update index.js

--- 03-Browsers/2020-12-14-Fetch/content/fetchAPI/index.js
+++ 03-Browsers/2020-12-14-Fetch/content/fetchAPI/index.js
@@ -1,6 +1,6 @@
 /* 
 Welcome Fbw3!
-Reference: https://mockapi.io/projects
+Reference: https://mockapi.io
 Reading assignment: https://javascript.info/promise-basics
 Reading assignment: http://ccoenraets.github.io/es6-tutorial/promises/#:~:text=A%20promise%20is%20a%20holder,support%20for%20promises%20to%20JavaScript.
 Reference: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
