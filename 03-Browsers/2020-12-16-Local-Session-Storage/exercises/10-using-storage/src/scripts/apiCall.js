e5ed019969496551bbcacf70cce55d6dea24dea2
Thomas Hofmann <thomas@savoy-truffle.de>
2020-12-16 08:57:32 +0100

    exercise on storage

---
+++ 03-Browsers/2020-12-16-Local-Session-Storage/exercises/10-using-storage/src/scripts/apiCall.js
@@ -0,0 +1,12 @@
+class Client {
+    constructor () {
+        //Your token goes here
+        this.token = "";
+    }
+
+    async getMovieData(movie) {
+        let response = await fetch(`http://www.omdbapi.com/?t=${movie}&apikey=${this.token}`);
+        let data = await response.json();
+        return data;
+    }
+}
