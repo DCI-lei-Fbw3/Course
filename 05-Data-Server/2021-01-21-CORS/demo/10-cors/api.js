75fde35a2a9cd9ec3f6beee7e1d662d7fc9a8bbb
Thomas Hofmann <thomas@savoy-truffle.de>
2021-01-20 16:43:15 +0100

    demo for CORS

---
+++ 05-Data-Server/2021-01-21-CORS/demo/10-cors/api.js
@@ -0,0 +1,19 @@
+// source:
+// https://expressjs.com/en/resources/middleware/cors.html
+
+var express = require('express')
+var cors = require('cors')
+var app = express()
+
+const port = 4000;
+
+//app.use(cors()) // enable cors globally
+
+app.get('/api/user', cors(), function (req, res, next) {
+	res.json( { id: 1, name: "alice" } );
+	next();
+})
+
+app.listen(port, function () {
+  console.log('CORS-enabled web server listening on port', port)
+})
