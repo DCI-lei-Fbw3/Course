6e98b5b29c3e131954aea5188f9f053303708a2a
Thomas Hofmann <thomas@savoy-truffle.de>
2021-01-20 17:56:37 +0100

    added exercise to handle non-responsive API-webserver

--- 05-Data-Server/2021-01-21-CORS/demo/10-cors/api.js
+++ 05-Data-Server/2021-01-21-CORS/demo/10-cors/api.js
@@ -9,6 +9,7 @@ const port = 4000;
 
 //app.use(cors()) // enable cors globally
 
+//                   enable CORS for this route
 app.get('/api/user', cors(), function (req, res, next) {
 	res.json( { id: 1, name: "alice" } );
 	next();
--- 05-Data-Server/2021-01-21-CORS/demo/10-cors/app.js
+++ 05-Data-Server/2021-01-21-CORS/demo/10-cors/app.js
@@ -1,6 +1,8 @@
 // source:
 // https://expressjs.com/en/resources/middleware/cors.html
 
+// a webserver for static files
+
 var express = require('express')
 var cors = require('cors')
 var app = express()
@@ -9,7 +11,7 @@ const port = 3000;
 
 app.use(cors()) // enable cors globally
 
-app.use('/static/', express.static('static'))
+app.use('/', express.static('static')) // serving files from the static subdirectory
 
 app.listen(port, function () {
   console.log('CORS-enabled web server listening on port', port)
