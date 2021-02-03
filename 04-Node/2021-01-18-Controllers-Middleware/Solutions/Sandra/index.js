8fc5fff8dbdaf07a7234c463e3d9826498fa72c4
Sandra Almoli <almolisandra@gmail.com>
2021-02-03 15:49:54 +0100

    mongodb

--- 04-Node/2021-01-18-Controllers-Middleware/Solutions/Sandra/index.js
+++ 04-Node/2021-01-18-Controllers-Middleware/Solutions/Sandra/index.js
@@ -1,7 +1,11 @@
+require('dotenv').config(); 
+
 const express = require("express"); 
 const bodyParser = require("body-parser"); 
 const cookieParser = require('cookie-parser')
 const app = express(); 
+
+require('./lib/database')
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cookieParser());
