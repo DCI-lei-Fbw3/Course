ffd7c35ce543102f21fc6e5dc5d40f1176c8e6d8
thisistaimur <59065240+thisistaimur@users.noreply.github.com>
2021-03-15 17:40:06 +0100

    Update react-auth.js

--- 06-Single-Page-Applications/2021-03-15-AuthwReact/content/react-auth.js
+++ 06-Single-Page-Applications/2021-03-15-AuthwReact/content/react-auth.js
@@ -1,6 +1,6 @@
 //Welcome Fbw3!
 // Topic: React - Authentication 
-//Reference: 
+//Reference: https://reactrouter.com/web/example/auth-workflow
 
 // Intro
 
@@ -10,15 +10,19 @@
     - This backend also in production on Heroku: https://mongo-express-auth.herokuapp.com
     - API Docs:
         - /register : POST --> email and pass to signup a new user
+            Takes: {email: String, Password: String}
         - /login: POST --> to recieve a JWT and login an existing user
+            Takes: {email: String, Password: String}
+        - /auth-endpoint: GET --> authorize token on protected React pages
+            Takes: {`Beare ${Token}}
 
     - Please spend 30-45 mins understanding the backend implementation, but remain aware
-    of the context here, which is to learn how auth work in React.
+    of the context here, which is to learn how auth works in React.
     */
 
 
     /* 
-    - We will be using the following three react hook:
+    - We will be using the following three react hooks:
         1) useState()
         2) useEffect()
         3) useContext() --> OPTIONAL for the bootsrap stuff
@@ -30,17 +34,15 @@
         4) Afternoon session: we will finish the react authentication
     */
 
-    //Steps to seure Authentication
+    //Steps to secure Authentication
         /* 
         - Step 1: To create a login/signup system 
-            1.a) Backend API, database, server, Tokens....(all that backend stuff)
-            1.b) Frontend pages and logic
+            1.1) Backend API, database, server, Tokens....(all that backend stuff)
+            1.1) Frontend pages and logic
         - Step 2: Define protected vs non-protected pages
+            2.2) Setting up React Router (ProtectedRoute)
         - Step 3: To protect the necessary Routes
-            3.1) make sure you cross-check your token (in a cookie) with your server
-        - Step 4: To use tokens for maintaining session
-        
-        
-        
+            3.1) make sure you cross-check your token (in a cookie) with your server otherwise there is a security loophole
+        - Step 4: To use tokens for maintaining session 
         */
 
