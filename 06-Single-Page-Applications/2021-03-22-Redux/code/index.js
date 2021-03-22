f7121916f35b8f60bab2f46c1062856281303f0a
thisistaimur <59065240+thisistaimur@users.noreply.github.com>
2021-03-22 10:40:51 +0100

    redux added

---
+++ 06-Single-Page-Applications/2021-03-22-Redux/code/index.js
@@ -0,0 +1,43 @@
+const {createStore} = require('redux');
+
+
+//Just the initial state of our data
+
+const initialState = {
+    age: 27
+}
+
+//Reducer: Follow the basic rules of reducers: (state, action) => newState, and update state immutably rather than mutating it directly.
+//1st argument: Default parameter from ES6, sets a default value in the parameters
+const myReducer = ((state = initialState, action)=>{
+//Create a copy of my initial state, since states are immutable objects
+const newState = {...state};
+
+if(action.type === 'ADD'){
+    newState.age += action.val;
+}
+
+if(action.type === 'SUBSTRACT'){
+    newState.age -= action.val;
+}
+
+return newState
+
+})
+
+
+const store = createStore(myReducer);//This creates the Redux Store, we need an initial state and a reducer function as the two arguments
+
+
+//Subscribe method subscribes us to any changes in the state (store)
+store.subscribe(() => {
+    console.log("State has been changed to:", store.getState())
+})
+
+//  When we call store.dispatch(action), we disptach an event to the reducer
+
+
+store.dispatch({type:'ADD',  val: 10});
+store.dispatch({type:'SUBSTRACT', val: 20});
+
+
---
+++ 06-Single-Page-Applications/2021-03-22-Redux/content/redux-intro.js
@@ -0,0 +1,40 @@
+//Weclcome FbW3
+//Topic: Redux -- Intro 
+//Reference: https://redux.js.org --> Redux
+//Reference 2: https://react-redux.js.org --> React-Redux
+
+//Redux 
+
+/* 
+- Redux itself has nothing to do with React per say, it has a standalone tool.
+- However, we do have a React specific bindings of Redux called "React-Redux".
+- I have heard many times, that Redux is the biggest barrier to writing React apps for devs.
+- Why should we use Redux at all, if its so complicated?
+    - This is definitely not because everyone of the internet using it.
+    - The reason Redux is useful is that it solves a problem. 
+    - This problem is not "state management". That is super vague, since React already
+    does state management. Redux does help to manage state, but that'S not the problem it
+    solves.
+    - Redux, is primarily, about data flow. It tries to streamline how data moves in 
+    your React application. 
+- Data flows in React from the props down to the component. Think of this as electricity, conencted by wires
+to the components. Data flows down and up these wires, but these wires cannot be run through thin air-
+they have to conencted between each component. 
+- Beacause redux is so good at managing how data flows between your components, it is quite useful
+as soon as you have tons and tons components relying on the same data.
+*/
+
+
+//How dow Redux work
+//Reference: https://redux.js.org/tutorials/fundamentals/part-2-concepts-data-flow#redux-terminology
+    /* 
+    - Redux has the following elements:
+        1) Store: this is the "box" that manages any updates to the state 
+        2) Reducer: this is the callback/function that literally updates the state based on events
+        3) Dispatcher: thhe dispatcher is responsible for communicating to the reducer to make certain changes
+        4) Actions:  You can think of an action as an event that describes something that happened in the application.
+
+    - The dispatcher dispatches an action to the reducer, the reducer takes the payload from the dispatcher and updates the state with the payload data. The reducer and the state sit in the redux store. 
+    */
+
+
